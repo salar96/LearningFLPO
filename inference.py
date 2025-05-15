248a9127978cb591ac6c14cee9c1fe744d6e9e06
Salar Basiri <salarbsr.1996@gmail.com>
2025-05-14 20:26:36 -0500

    new file:   BeamSearch.py 	new file:   Saved Models/VRPNet_L_lr1e-04_bs256_ep80000_samples20480000_cities10_inputdim2_workers0_hidden64_enc6_dec3_heads8_dropout0.10_beta10000.000-10000.0_train_supervised_2025_05_13_11_34_05best_model.pth 	modified:   VRP_Net_L.py 	new file:   __pycache__/BeamSearch.cpython-310.pyc 	modified:   __pycache__/LSE_net.cpython-310.pyc 	modified:   __pycache__/VRP_Net_L.cpython-310.pyc 	new file:   __pycache__/inference.cpython-310.pyc 	modified:   __pycache__/utils.cpython-310.pyc 	new file:   inference.py 	modified:   main.ipynb 	modified:   utils.py

---
+++ inference.py
@@ -0,0 +1,23 @@
+import torch
+from BeamSearch import beam_search
+
+def inference(data , model, method = 'BeamSearch'):
+
+    num_data , num_cities , _ = data.shape
+    device = data.device
+    
+    if method == 'Greedy':
+        actions = torch.zeros(num_data, num_cities, 1).to(device)
+        for t in range(1, num_cities):
+            E = model.encoder(data)
+            prev_chosen_indices = actions[:,t-1,0].unsqueeze(1).long()
+            _ , outs = model.decoder(E , prev_chosen_indices, relevance_mask = None)
+            actions[:, t, 0] = torch.argmax(outs, dim=-1)
+            
+    elif method == 'BeamSearch':
+        seq, scores = beam_search(model,data,beam_width=min(num_cities,5))
+        actions = seq[:,0,:]
+    else:
+        raise "Wrong Method!"
+    
+    return actions
