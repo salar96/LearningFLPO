2289f328d76ad9538968406778f4de65d77c04d0
Salar Basiri <salarbsr.1996@gmail.com>
2025-07-20 15:40:58 -0500

    Staying at depo after reaching depo in inference
    Improves masking logic in Greedy and sampling methods to better handle depot selection and mask updates. Removes unused relevance mask generator and streamlines code formatting for clarity.

--- inference.py
+++ inference.py
@@ -1,33 +1,39 @@
 import torch
 from BeamSearch import beam_search
 from RelevanceMaskGenerator import RelevanceMaskGenerator
+def inference(data , model, method = 'BeamSearch'):
 
-
-def inference(data, model, method="BeamSearch"):
-
-    num_data, num_cities, _ = data.shape
+    num_data , num_cities , _ = data.shape
     device = data.device
     P_s = None
-    if method == "Greedy":
-        actions = torch.zeros(num_data, num_cities, 1).to(device)
+    if method == 'Greedy':
+        actions = torch.zeros(num_data, num_cities,1).to(device)
         E = model.encoder(data)
-        depo = data[:, -1, :]
+        depo = data[:, -1, :] 
         for t in range(1, num_cities):
 
-            indices = actions[:, t - 1, 0].long()
-            stokens = data[torch.arange(num_data).unsqueeze(1), indices.unsqueeze(1), :]
+            indices = actions[:,t-1,0].long()
+            stokens = data[
+                torch.arange(num_data).unsqueeze(1), indices.unsqueeze(1), :
+                ]
             with torch.no_grad():
                 m1 = torch.zeros(num_data, num_cities).to(device)
-                m1[torch.arange(num_data).unsqueeze(1), actions[:, :t, 0].long()] = 1
-                m2 = RelevanceMaskGenerator(data, indices).float()
+                m1[torch.arange(num_data).unsqueeze(1), actions[:,:t,0].long()] = 1
+                end_selected = indices == (
+                num_cities - 1
+                )  # shows if we are already at the depot
+                m1[end_selected, :-1] = (
+                    1  # mask all except last node (depot) for batches at depot
+                )
+                # m2 = RelevanceMaskGenerator(data, indices).float()
                 # relevance_mask = ((m1 + m2) > 0).float()
                 relevance_mask = m1
                 relevance_mask[:, -1] = 0  # Last node (depot) should not be selected
-            _, outs = model.decoder(E, stokens.squeeze(1), depo, mask=relevance_mask)
+            _ , outs = model.decoder(E , stokens.squeeze(1), depo, mask = relevance_mask)
             actions[:, t, 0] = torch.argmax(outs, dim=-1)
 
-    elif method == "BeamSearch":
-        actions, scores = beam_search(model, data, beam_width=5)
+    elif method == 'BeamSearch':
+        actions, scores = beam_search(model,data,beam_width=5)
     elif method == "sampling":
         actions = torch.zeros(num_data, num_cities, 1).to(device)
         P_s = torch.zeros(num_data, num_cities, num_cities).to(device)
@@ -35,21 +41,29 @@ def inference(data, model, method="BeamSearch"):
         depo = data[:, -1, :]
         for t in range(1, num_cities):
 
-            indices = actions[:, t - 1, 0].long()
-            stokens = data[torch.arange(num_data).unsqueeze(1), indices.unsqueeze(1), :]
+            indices = actions[:,t-1,0].long()
+            stokens = data[
+                torch.arange(num_data).unsqueeze(1), indices.unsqueeze(1), :
+                ]
             with torch.no_grad():
                 m1 = torch.zeros(num_data, num_cities).to(device)
-                m1[torch.arange(num_data).unsqueeze(1), actions[:, :t, 0].long()] = 1
-                m2 = RelevanceMaskGenerator(data, indices).float()
+                m1[torch.arange(num_data).unsqueeze(1), actions[:,:t,0].long()] = 1
+                end_selected = indices == (
+                num_cities - 1
+                )  # shows if we are already at the depot
+                m1[end_selected, :-1] = (
+                    1  # mask all except last node (depot) for batches at depot
+                )
+                # m2 = RelevanceMaskGenerator(data, indices).float()
                 # relevance_mask = ((m1 + m2) > 0).float()
                 relevance_mask = m1
                 relevance_mask[:, -1] = 0  # Last node (depot) should not be selected
-            _, outs = model.decoder(E, stokens.squeeze(1), depo, mask=relevance_mask)
+            _ , outs = model.decoder(E , stokens.squeeze(1), depo, mask = relevance_mask)
             with torch.no_grad():
-                a = torch.multinomial(outs, num_samples=1).squeeze(-1)
+                a  = torch.multinomial(outs, num_samples=1).squeeze(-1)
                 actions[:, t, 0] = a
             P_s[:, t, :] = outs
     else:
         raise ValueError("Wrong Method!")
 
-    return P_s, actions
+    return P_s , actions
